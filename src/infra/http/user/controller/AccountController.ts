627322e4e319a755e99030fc55ec5168ed830a5e
Giovani-Coelho <giovanicoelho2004@hotmail.com>
2023-05-08 18:23:20 -0300

    controller refactoring

--- src/infra/http/user/controller/AccountController.ts
+++ src/infra/http/user/controller/AccountController.ts
@@ -10,38 +10,35 @@ const accountRepository = new AccountRepository();
 const accountUseCase = new CreateAccount(accountRepository);
 
 class AccountController {
-  public async create(req: Request, res: Response) {
+  public async create(req: Request, res: Response<ISuccessResponse<IAccountResponse> | IErrorResponse>):
+    Promise<Response<ISuccessResponse<IAccountResponse> | IErrorResponse>> {
     const { name, email, password } = req.body
 
-    await accountUseCase.execute({ name, email, password })
+    const account = await accountUseCase.execute({ name, email, password })
 
-    return res.status(201).send()
-
-    // try {
-    //   return res.status(201).send({
-    //     status: 201,
-    //     body: {
-    //       account: {
-    //         name: account.name,
-    //         email: account.email,
-    //         password: account.password,
-    //       }
-    //     },
-    //     success: true,
-    //     error: false
-    //   })
-    // } catch (error) {
-    // : Promise<Response<ISuccessResponse<IAccountResponse> | IErrorResponse>>
-
-    // return res.status(400).send({
-    //   status: 400,
-    //   success: false,
-    //   error: true,
-    //   message: error.message,
-    // })
+    try {
+      return res.status(201).send({
+        status: 201,
+        body: {
+          account: {
+            name: account.name,
+            email: account.email,
+            password: account.password,
+          }
+        },
+        success: true,
+        error: false,
+      })
+    } catch (error) {
+      return res.status(400).send({
+        status: 400,
+        success: false,
+        error: true,
+        message: "a"
+      })
+    }
   }
 }
-
 
 
 export { AccountController }
