e9e4d8fadf61804c6675fd1d8a831da546982a7b
Giovani-Coelho <giovanicoelho2004@hotmail.com>
2023-06-06 23:57:57 -0300

    feat: delete route promise & readme

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ Principais tecnologias usadas para desenvolver esta API.
 ✅ - Deve ser possivel criar uma conta.<br>
 ✅ - Deve ser possivel o usuario se autenticar.<br>
 ✅ - Deve ser possivel o usuario publicar um produto.<br>
-[X] - Deve ser possivel o usuario deletar um produto.<br>
+✅ - Deve ser possivel o usuario deletar um produto.<br>
 ✅ - Deve ser possivel o usuario listar todos os seus produto.<br>
 ✅ - Deve ser possivel o usuario listar todos seus produtos.<br>
 ✅ - Deve ser possivel o usuario criar um pedido.<br>
@@ -41,8 +41,8 @@ Principais tecnologias usadas para desenvolver esta API.
 ✅ - Nao deve ser possivel se cadastrar com o mesmo e-mail.<br>
 ✅ - Nao deve ser possivel criar um produto se o usuario nao existir.<br>
 ✅ - Nao deve ser possivel listar produtos de um usuario inexistente.<br>
-[X] - Nao deve ser possivel deletar um produto inexistente.<br>
-[x] - Nao deve ser possivel deletar um produto que nao pertence a um usuario.<br>
+✅ - Nao deve ser possivel deletar um produto inexistente.<br>
+✅ - Nao deve ser possivel deletar um produto que nao pertence a um usuario.<br>
 ✅ - Nao deve ser possivel o usuario listar produtos caso nao tenha.<br>
 ✅ - Nao deve ser possivel criar um pedido usando um usuario inexistente.<br>
 ✅ - Nao deve ser possivel criar um pedido se for passado um valor de status incorreto.<br>
--- src/infra/http/product/controller/DeleteProductController.ts
+++ src/infra/http/product/controller/DeleteProductController.ts
@@ -1,15 +1,19 @@
+import { ISuccessResponse } from './../../interfaceResponse/ISucessResponse';
 import { DeleteProduct } from "@/app/product/delete/DeleteProduct";
 import { ProductIsNotAccount } from "@/domain/product/ProductIsNotAccount";
 import { ProductNotFound } from "@/domain/product/ProductNotFound";
 import { ProductRepository } from "@/infra/repositories/product/ProductRepository";
 import { Request, Response } from "express";
 import { ZodError, z } from "zod";
+import { IErrorResponse } from '../../interfaceResponse/IErrorResponse';
 
 const productRepository = new ProductRepository();
 const deleteProduct = new DeleteProduct(productRepository);
 
 class DeleteProductController {
-  public async handle(req: Request, res: Response) {
+  public async handle(req: Request, res: Response<ISuccessResponse<null> | IErrorResponse>):
+    Promise<Response<ISuccessResponse<null> | IErrorResponse> | undefined> {
+
     const accountSchema = z.object({
       account_id: z.string().uuid()
     })
@@ -19,25 +23,26 @@ class DeleteProductController {
     })
 
     try {
+
       const { account_id } = accountSchema.parse(req.params);
       const { product_id } = productSchema.parse(req.body);
 
       await deleteProduct.execute(account_id, product_id);
 
-      return res.status(204).send({ status: 204, message: "Product deleted successfully" });
+      return res.status(204).send();
 
     } catch (error) {
+
       if (error instanceof ZodError)
-        return res.status(400).json({ status: 400, message: "Incorrect Credentials!" })
+        return res.status(400).json({ status: 400, error: "Incorrect Credentials!" })
 
       if (error instanceof ProductNotFound)
-        return res.status(400).json({ status: 400, message: error.message })
+        return res.status(400).json({ status: 400, error: error.message })
 
       if (error instanceof ProductIsNotAccount)
-        return res.status(400).json({ status: 400, message: error.message })
+        return res.status(400).json({ status: 400, error: error.message })
 
-
-      return res.status(400).json({ status: 400, message: "Internal Server Error!" })
+      return res.status(400).json({ status: 400, error: "Internal Server Error!" })
     }
   }
 }
