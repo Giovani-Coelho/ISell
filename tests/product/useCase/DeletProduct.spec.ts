327906b6342354366192cc566b5ba1779b9030f6
Giovani-Coelho <giovanicoelho2004@hotmail.com>
2023-06-06 22:54:15 -0300

    test: This product does not belong to this user

--- tests/product/useCase/DeletProduct.spec.ts
+++ tests/product/useCase/DeletProduct.spec.ts
@@ -1,6 +1,7 @@
 import { CreateAccount } from "@/app/account/create/CreateAccount";
 import { CreateProduct } from "@/app/product/create/CreateProduct";
 import { DeleteProduct } from "@/app/product/delete/DeleteProduct";
+import { ProductIsNotAccount } from "@/domain/product/ProductIsNotAccount";
 import { ProductNotFound } from "@/domain/product/ProductNotFound";
 import { AccountRepositoryInMemory } from "@/infra/repositories/account/AccountRepositoryInMemory";
 import { ProductRepositoryInMemory } from "@/infra/repositories/product/ProductRepositoryInMemory";
@@ -54,5 +55,26 @@ describe("Delete Product", () => {
     expect(async () => {
       await deleteProduct.execute(account.id as string, "111");
     }).rejects.toThrow(ProductNotFound);
+  })
+
+  it("Should not be able to delete if the account_id does not exist", async () => {
+    const account = await createAccount.execute({
+      name: "Giovani Coelho",
+      email: "giovanicoelho@hotmail.com",
+      password: "123456"
+    })
+
+    const product = await createProduct.execute({
+      name: "lapis",
+      price: 1.90,
+      amount: 2,
+      description: "",
+      available: true,
+      account_id: account.id as string
+    })
+
+    expect(async () => {
+      await deleteProduct.execute("hahaha" as string, product.id);
+    }).rejects.toThrow(ProductIsNotAccount);
   })
 })
